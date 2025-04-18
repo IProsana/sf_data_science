e6e17894c139f5bcb0401cc2c71e8c9f857d61d1
IProsana <strelka@inbox.ru>
2025-04-18 19:59:32 +0300

    add requirements.txt edit README.md

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -1,9 +1,11 @@
-"""Игра угадай число.
-Компьютер сам загадывает и угадывает число
+"""Игра угадай число
+Компьютер сам загадывает и сам угадывает число
 """
+
 import numpy as np
 
-def random_predict(number:int=1) -> int:
+
+def random_predict(number: int = 1) -> int:
     """Рандомно угадываем число
 
     Args:
@@ -12,40 +14,96 @@ def random_predict(number:int=1) -> int:
     Returns:
         int: Число попыток
     """
-
     count = 0
 
     while True:
         count += 1
-        predict_number = np.random.randint(1, 101) # предполагаемое число
+        predict_number = np.random.randint(1, 101)  # предполагаемое число
         if number == predict_number:
-            break # выход из цикла, если угадали
-    return(count)
+            break  # выход из цикла если угадали
+    return count
 
-print(f'Количество попыток: {random_predict()}')
 
-def score_game(random_predict) -> int:
-    """За какое количество попыток в среднем из 1000 подходов угадывает наш алгоритм
+def game_core_v2(number: int = 1) -> int:
+    """Сначала устанавливаем любое random число, а потом уменьшаем
+    или увеличиваем его в зависимости от того, больше оно или меньше нужного.
+       Функция принимает загаданное число и возвращает число попыток
 
     Args:
-        random_predict ([type]): функция угадывания
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    count = 0
+    predict = np.random.randint(1, 101)
+
+    while number != predict:
+        count += 1
+        if number > predict:
+            predict += 1
+        elif number < predict:
+            predict -= 1
+
+    return count
+
+def game_core_v3(number: int = 1) -> int:
+    """Угадываем число бинарным поиском.
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+
+    count = 0
+    min_value = 0
+    max_value = 101
+    #print("загадано число: ", number)
+ 
+
+    while min_value <= max_value:
+        count += 1
+        
+        mid_value = (min_value + max_value) // 2
+        
+        if mid_value == number:
+            #print("Найдено число: ", mid_value)
+            break
+           
+        if mid_value > number:
+            max_value = mid_value - 1
+        else:
+            min_value = mid_value + 1
+
+
+    return count
+
+# Функция для оценки
+def score_game(name_func) -> int:
+    """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
+
+    Args:
+        name_func ([type]): функция угадывания
 
     Returns:
         int: среднее количество попыток
     """
-
-    count_ls = [] # список для сохранения количества попыток
-    np.random.seed(1) # фиксируем сид для воспроизводимости
-    random_array = np.random.randint(1, 101, size=(1000)) # загадали список чисел
+    count_ls = []
+    #np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
 
     for number in random_array:
-        count_ls.append(random_predict(number))
+        count_ls.append(name_func(number))
 
-    score = int(np.mean(count_ls)) # находим среднее количество попыток
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
+    return score
 
-    print(f'Ваш алгоритм угадывает число в среднем за: {score} попыток')
-    return(score)
 
-# RUN
-if __name__ == '__main__':
+if __name__ == "__main__":
+    # RUN
     score_game(random_predict)
+    score_game(game_core_v2)
+    score_game(game_core_v3)
+    
